627079759d0a4d21e169d90275bc5c9a9c7336b9
AntonVoronko <voronyj@mail.ru>
2017-02-28 20:28:41 +0200

    node controllers updated

--- controllers/feedbackController.js
+++ controllers/feedbackController.js
@@ -11,53 +11,53 @@ var connection = mysql.createConnection({
 
 module.exports = {
   getFeedbacks: function (callback) {
-	connection.query('SELECT * FROM feedback;', function (err, rows) {
-	  if (!err) {
-	    callback(null, { feedback: rows });
-	  }
-	  else {
-	  	console.log('error');
-	    console.log(err);
-	  }
-	})
+  connection.query('SELECT * FROM feedback;', function (err, rows) {
+    if (!err) {
+      callback(null, { feedback: rows });
+    }
+    else {
+      console.log('error');
+      console.log(err);
+    }
+  })
   },
   addFeedback: function (feedback, callback) {
-	connection.query('INSERT INTO feedback(name, text, date)' +
-		'VALUES (' + feedback.name + ', ' + feedback.text + ', ' + feedback.date + '));', function (err, rows, fields) {
-	  if (!err) {
-	  	console.log('rows');
-	  	console.log(rows);
-	    callback(null, { feedback: rows });
-	  }
-	  else {
-	    console.log(err);
-	  }
-	});
+  connection.query('INSERT INTO feedback(name, text, date)' +
+    'VALUES (' + feedback.name + ', ' + feedback.text + ', ' + feedback.date + '));', function (err, rows, fields) {
+    if (!err) {
+      console.log('rows');
+      console.log(rows);
+      callback(null, { feedback: rows });
+    }
+    else {
+      console.log(err);
+    }
+  });
   },
   updateFeedback: function (id, feedback, callback) {
-  	connection.query('UPDATE feedback SET name = "' + feedback.name + 
-  		'", text = "'+ feedback.text + '" WHERE id=' + id + ';', function (err, rows) {
-	  if (!err) {
-	  	console.log('rows');
-	  	console.log(rows);
-	    callback(null, feedback);
-	  }
-	  else {
-	    console.log(err);
-	  }
-	});
+    connection.query('UPDATE feedback SET name = "' + feedback.name + 
+      '", text = "'+ feedback.text + '" WHERE id=' + id + ';', function (err, rows) {
+    if (!err) {
+      console.log('rows');
+      console.log(rows);
+      callback(null, feedback);
+    }
+    else {
+      console.log(err);
+    }
+  });
   },
   deleteFeedback: function (id, callback) {
-  	connection.query('DELETE FROM  feedback WHERE id =' + id + ';', function (err, rows) {
-	  if (!err) {
-	  	console.log('rows');
-	  	console.log(rows);
-	    callback(null, feedback);
-	  }
-	  else {
-	    console.log(err);
-	  }
-	});
+    connection.query('DELETE FROM  feedback WHERE id =' + id + ';', function (err, rows) {
+    if (!err) {
+      console.log('rows');
+      console.log(rows);
+      callback(null, feedback);
+    }
+    else {
+      console.log(err);
+    }
+  });
   }
 
 };
