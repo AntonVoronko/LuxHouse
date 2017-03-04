7a0bafc47e32b3053b3b31d6e2a17f7ee94a272f
AntonVoronko <voronyj@mail.ru>
2017-03-04 16:14:04 +0200

    added carousel

--- app/main/js/mainController.js
+++ app/main/js/mainController.js
@@ -1,6 +1,11 @@
+require('jquery');
+require('owl.carousel');
+
 module.exports = mainController;
 
 function mainController () {
-	
+	setTimeout(function(){
+		$('.owl-carousel').owlCarousel();
+	}, 2000);
 };
 mainController.$inject = [];
