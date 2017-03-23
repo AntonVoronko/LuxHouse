302372ea897145550c815c09e8c2c80a3b2594f8
AntonVoronko <voronyj@mail.ru>
2017-03-23 20:49:04 +0200

    added upload module

--- app.js
+++ app.js
@@ -14,13 +14,46 @@ var news = require('./controllers/newsController');
 var feedbacks = require('./controllers/feedbackController');
 var works = require('./controllers/worksController');
 var auth = require('./controllers/authorizationController');
+var multer = require('multer');
 
- app.use(express.static(__dirname + '/public'));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.use(bodyParser.json());
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads/')
+  },
+  filename: function (req, file, cb) {
+    var datetimestamp = Date.now();
+    cb(null, file.fieldname + '-' + datetimestamp + '.' + file.originalname.split('.')[file.originalname.split('.').length -1])
+  }
+});
+
+var upload = multer({
+  storage: storage
+}).single('file');
 
 app.get('/', function (req, res) {
   res.render('index', function (err, html) {
     res.send(html);
   });
+});
+
+app.post('/upload', function(req, res) {
+  upload(req,res,function(err){
+    if(err){
+      res.json({error_code:1,err_desc:err});
+      return;
+    }
+    res.json({error_code:0,err_desc:null});
+  })
 });
 
 app.route('/api/news')
